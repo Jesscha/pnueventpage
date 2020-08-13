c0e8d3b615421adf3b933f21a281ce9bb77f9942
jessecha <jessecha222@gmail.com>
2020-08-13 22:22:57 +0900

    add question logic

--- pnumbti/src/App.js
+++ pnumbti/src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ResultPage from './pages/ResultPage/ResultPage';
 import QuestionPage from './pages/QuestionPage/QuestionPage';
 import HomePage from './pages/HomePage/HomePage';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/result">
-          <ResultPage />
-        </Route>
-        <Route path="/question">
-          <QuestionPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="container">
+      <Router>
+        <Switch>
+          <Route path="/result">
+            <ResultPage />
+          </Route>
+          <Route path="/question">
+            <QuestionPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- pnumbti/src/pages/HomePage/HomePage.jsx
+++ pnumbti/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import './HomePage.scss'
+import { Link } from 'react-router-dom';
 
 
 function HomePage() {
   return (
-    <div>
-      home
+    <div classNameˇ="home-container">
+      <h1>Pnuu</h1>
+      <p>
+        여느 때와 같은 평일 저녁,
+        당신은 친구에게서 초대장과 봉투를 받게 됩니다.
+
+        "내 생일 파티는 금요일에 여기서 해, 꼭 와줘."
+        라고 말하면서 말이죠.
+
+        호기심이 든 당신은 바로 봉투 안을 열어보았습니다.
+      </p>
+
+      <Link to="/question">
+        시작하기
+      </Link>
+
     </div>
   );
 }
