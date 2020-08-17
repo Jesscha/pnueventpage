1a099fd99939316c277cba3c87e63f1d2ad03163
jessecha <jessecha222@gmail.com>
2020-08-17 20:27:16 +0900

    add result

--- pnumbti/src/pages/ResultPage/ResultPage.jsx
+++ pnumbti/src/pages/ResultPage/ResultPage.jsx
@@ -1,38 +1,187 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 
 // 16가지 결과를 kv로 저장해 놓는다.
 
-
 function ResultPage() {
 
   const { mbti } = useParams();
 
-
-
-
-
+  const mbtiData = resultData[mbti.trim()];
+  console.log(resultData["INTP"])
+  console.log({ mbti })
   return (
     <div>
-      ResultPage {mbti}
+      ResultPage {mbti} {mbtiData.fitFriendsType}
     </div>
   );
 }
 
 
-// const resultData = [{
-//   type: "INTJ",
-//   mainImage: "",
-//   inFriendsBirthDay: "",
-//   inMyBirthDay: "",
-//   fitFriendsType: "ENTP",
-//   fitFriendsText: "최고최고",
+const resultData = {
+  INTJ: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
 
-//   unfitFriendsType: "ISTJ",
-//   unfitFriendsText: "안놀아 ㅠㅠ",
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  INTP: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
 
-// }]
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ENTJ: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ENTP: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  INFJ: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  INFP: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ENFJ: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ENFP: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ISTJ: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ISFJ: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ESTJ: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ESFJ: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ISTP: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ISFP: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ESTP: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+  ESFP: {
+    mainImage: "",
+    inFriendsBirthDay: "",
+    inMyBirthDay: "",
+    fitFriendsType: "ENTP",
+    fitFriendsText: "최고최고",
+
+    unfitFriendsType: "ISTJ",
+    unfitFriendsText: "안놀아 ㅠㅠ",
+  },
+
+}
 
 
 
