076d8d19bff2288308474b2b6f25c7b9c08f3843
jessecha <jessecha222@gmail.com>
2020-11-15 02:11:46 +0900

    결과 이미지

--- pnumbti/src/pages/ResultPage/ResultPage.jsx
+++ pnumbti/src/pages/ResultPage/ResultPage.jsx
@@ -1,23 +1,25 @@
 import React, { } from 'react';
 
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, withRouter } from 'react-router-dom';
 import resultData from '../../asset/resultData';
+import './ResultPage.scss'
+import { stringBreak } from '../../utils/utils';
 
 
 
 // 16가지 결과를 kv로 저장해 놓는다.
 
-function ResultPage() {
+function ResultPage({ history }) {
 
   const { mbti } = useParams();
 
   const mbtiData = resultData[mbti.trim()];
   const allArr = makeResultArr();
-  const resultKeys =Object.keys(resultData);
+  const resultKeys = Object.keys(resultData);
 
-  function makeResultArr(){
+  function makeResultArr() {
     const newArr = []
-    for (let key of Object.keys(resultData)){
+    for (let key of Object.keys(resultData)) {
       newArr.push(resultData[key])
     }
     return newArr
@@ -26,113 +28,114 @@ function ResultPage() {
   // console.log(allArr)
   // console.log(resultKeys)
   return (
-    <>
-
-      {
-        mbti.trim() === "all"?
-        <div className="result-all">
-          {
-            allArr.map((type, i) =>{
-              console.log(type)
-              return(
-                <div>
-                  <Link
-                  key = {i}
-                  to ={`/result/${resultKeys[i]}`}
-                  >
-                  {type.nickName}
-                  </Link>
-                </div>
-                
-              )
-            })
-          }
-        </div>
-        :
-      <div>
-      ResultPage {mbti} {mbtiData.fitFriendsType}
-      <div style={{ width: "100%" }}
-      >
-        <div className="result-head">
-          <div>
-            {mbtiData.nickNameDesc}
-          </div>
-          <div>
-            {mbtiData.nickName}
-          </div>
-        </div>
-        {mbtiData.mainImage && <img style={{ width: "100%" }} src={mbtiData.mainImage} alt="" /> }  
-        <div className="result-main-desc">
-            {mbtiData.mainDesc}
-        </div>
-        <div className="result-second-desc">
-          <div className="inFriendsBirthDay">
-            <div className="intro">
-              친구 생일에 나는 
-            </div>
-           
-            {mbtiData.inFriendsBirthDay}
-          </div>
-
-          <div className="inMyBirthDay">
-            <div className="intro">
-              내 생일에 나는 
-            </div>
-              {mbtiData.inMyBirthDay}
-          </div>
-           
-        </div>
-        <div className="result-third-desc">
-          <div>
-            궁합
-          </div>
-          <div className="result-relation" >
-            <div className="left"
-            >
-              <Link
-              to ={`/result/${mbtiData.fitFriendsType}`}
-              >
-                {mbtiData.fitFriendsNickName}
-              </Link>
-            </div>
-            <div className="right"
-            >
-              <Link
-              to ={`/result/${mbtiData.unfitFriendsType}`}
-              >
-                {mbtiData.unfitFriendsNickName}
-              </Link>
-            
-
-            </div>
-
-          </div>
-        </div>
-        <div className="result-go-toPnuu">
-        내 생일 펀딩해 보기
-        </div>
-        <div className="result-go-toPnuu">
-          다가오는 친구 생일 제대로 축하해주기
-        </div>
-        <div className ="result-move-to-all">
-        <Link
-              to ={`/result/all`}
-              >
-                전체 결과 보기
-        </Link>
-          
-
-        </div>
-        
+    <div className="resultPage">
+      <div className="logo">
+        pnuu
       </div>
+      {
+        mbti.trim() === "all" ?
+          <div className="result-all">
+            {
+              allArr.map((type, i) => {
+                return (
+                  <div
+                    onClick={() => {
+                      history.push(`/result/${resultKeys[i]}`)
+                    }}
+                  >
+                    {type.nickName}
+                  </div>
+                )
+              })
+            }
+          </div>
+          :
+          <div>
+            <div style={{ width: "100%" }}
+            >
+              <div className="result-head">
+                <div
+                  className="subText"
+                >
+                  {mbtiData.nickNameDesc}
+                </div>
+                <div
+                  className="mainText">
+                  {mbtiData.nickName}
+                </div>
+              </div>
+              {mbtiData.mainImage && <img style={{ width: "100%" }} src={mbtiData.mainImage} alt="" />}
+              <div className="result-main-desc">
+                {stringBreak(mbtiData.mainDesc)}
+              </div>
+              <div className="result-second-desc">
+                <div className="inFriendsBirthDay">
+                  <div className="intro">
+                    친구 생일에 나는
+            </div>
+
+                  {stringBreak(mbtiData.inFriendsBirthDay)}
+                </div>
+
+                <div className="inMyBirthDay">
+                  <div className="intro">
+                    내 생일에 나는
+            </div>
+                  {stringBreak(mbtiData.inMyBirthDay)}
+                </div>
+
+              </div>
+              <div className="result-third-desc">
+                <div>
+                  궁합
+          </div>
+                <div className="result-relation" >
+                  <div className="left"
+                  >
+                    <Link
+                      to={`/result/${mbtiData.fitFriendsType}`}
+                    >
+                      {mbtiData.fitFriendsNickName}
+                    </Link>
+                  </div>
+                  <div className="right"
+                  >
+                    <Link
+                      to={`/result/${mbtiData.unfitFriendsType}`}
+                    >
+                      {mbtiData.unfitFriendsNickName}
+                    </Link>
+
+
+                  </div>
+
+                </div>
+              </div>
+              <div className="result-go-toPnuu">
+                내 생일 펀딩해 보기
+        </div>
+              <div className="result-go-toPnuu">
+                다가오는 친구 생일 제대로 축하해주기
+        </div>
+              <div className="result-move-to-all">
+                <Link
+                  to={`/result/all`}
+                >
+                  전체 결과 보기
+        </Link>
+
+
+              </div>
+
+            </div>
+
+          </div>
+
+
+
+      }
 
     </div>
-
-
-        
-      }
-    
-    </>
   );
 }
 
@@ -143,4 +146,4 @@ function ResultPage() {
 
 
 
-export default ResultPage;
+export default withRouter(ResultPage);
