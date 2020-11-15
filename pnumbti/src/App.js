6d5c336050c35adcc3c7a9a10ec051c8c600edc3
jessecha <jessecha222@gmail.com>
2020-11-15 21:54:06 +0900

    favicon, 타이틀 변경

--- pnumbti/src/App.js
+++ pnumbti/src/App.js
@@ -5,10 +5,15 @@ import QuestionPage from './pages/QuestionPage/QuestionPage';
 import HomePage from './pages/HomePage/HomePage';
 import './App.css';
 import Survey from './pages/Survey/Survey';
-
+import { Helmet } from 'react-helmet';
 function App() {
   return (
     <div className="container">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Pnuu-생일로 알아보는 당신의 속마음</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <Router>
         <Switch>
           <Route path="/result/:mbti">
