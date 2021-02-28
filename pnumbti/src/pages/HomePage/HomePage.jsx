becf45e36842361f561639e6f1ada7a941463be8
Jesse Cha <jessecha222@gmail.com>
2021-02-28 16:48:49 +0900

    init design

--- pnumbti/src/pages/HomePage/HomePage.jsx
+++ pnumbti/src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import './HomePage.scss'
+import React from "react";
+import "./HomePage.scss";
 import { withRouter } from "react-router-dom";
 // import { Link } from 'react-router-dom';
-
+import mainGif from "../../asset/main.gif";
+import logo from "../../asset/logo_black.svg";
 
 function HomePage({ history }) {
   return (
     <div className="home-container">
-      <div className="home-logo">Pnuu</div>
+      <div className="home-logo">
+        <img src={logo} alt="" />
+      </div>
+      <img className="homeMainImage" src={mainGif} alt="" />
+
       <h1 className="home-headText">
-        생일로 알아보는 <br />
-        내 속마음
+        <span className="home-headText-point">생일</span>로 알아보는 <br />내
+        속마음
       </h1>
 
-      <p className="home-mainText">
+      <p className="homeDescText">생일 파티에서 내 성격은?</p>
+
+      <button
+        onClick={() => {
+          history.push("/question");
+        }}
+        className="homeCTAButton"
+      >
+        테스트 시작하기
+      </button>
+
+      {/* <p className="home-mainText">
 
         "내 생일 파티는 금요일에 여기서 해, <br />
         꼭 와줘."<br />
@@ -31,14 +47,9 @@ function HomePage({ history }) {
         }}
       >
         시작하기
-      </div>
-
-
-
+      </div> */}
     </div>
   );
 }
-
-
 
 export default withRouter(HomePage);
